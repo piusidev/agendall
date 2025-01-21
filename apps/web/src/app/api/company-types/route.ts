491e4ec856da2396f142a8c7300169a3386132a7
Leonardo Piusi <leopidev@gmail.com>
2025-01-21 01:14:44 -0300

    feat: route to get company types

---
+++ apps/web/src/app/api/company-types/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server'
+import { createClient } from '@repo/supabase/client'
+
+export async function GET() {
+  try {
+    const client = createClient()
+
+    const { data: companyTypes, error } = await client
+      .from('company_types')
+      .select('*')
+
+    if (error) {
+      return NextResponse.json(
+        { message: 'Failed to fetch company types', error: error.message },
+        { status: 500 },
+      )
+    }
+
+    if (!companyTypes || companyTypes.length === 0) {
+      return NextResponse.json(
+        { data: [], message: 'No company types available' },
+        { status: 204 },
+      )
+    }
+
+    return NextResponse.json(
+      { data: companyTypes, message: null },
+      { status: 200 },
+    )
+  } catch (err) {
+    return NextResponse.json(
+      { message: 'An unexpected error occurred', error: err },
+      { status: 500 },
+    )
+  }
+}
