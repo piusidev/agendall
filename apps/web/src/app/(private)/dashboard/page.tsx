63f121f73b5866e499f41b22e7a6fa93bfe16d10
Leonardo Piusi <leopidev@gmail.com>
2025-02-02 19:31:11 -0300

    feat: dashboard structure

---
+++ apps/web/src/app/(private)/dashboard/page.tsx
@@ -0,0 +1,38 @@
+import { Metadata } from 'next'
+
+import { AppSidebar } from '@/modules/shared/components/sidebar/app-sidebar'
+
+import { Separator } from '@agendall/ui/separator'
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@agendall/ui/sidebar'
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
+
+export default async function Page() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
